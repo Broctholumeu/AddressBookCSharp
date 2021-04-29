f4b75a5c1141cc12998bae79382479b1c60ee96b
Broctholumeu <Brocksqd@gmail.com>
2021-04-29 15:38:51 -0400

    Add project files.

---
+++ AddressBook/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Category
+    {
+        //Primary Key
+        public int Id { get; set; }
+        //Identify Person
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name ="Category Picture")]
+        public byte[] CategoryPicture { get; set; }
+        public string CategoryContentType { get; set; }
+
+        //Defining a FOREIGN KEY
+        public ICollection<Contact> Contact { get; set; } = new HashSet<Contact>();
+
+
+    }
+}
---
+++ AddressBook/Models/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Contact
+    {
+       
+        public int Id { get; set; }
+        //Ties into parent doc
+        public int CategoryId { get; set; }
+        //identify unique id
+        public int NameId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public int HomePhone { get; set; }
+        public int CellPhone { get; set; }
+        public int FaxNumber { get; set; }
+        public string Email { get; set; }
+
+        [Display(Name ="Contact Pic")]
+        public byte[] Picture { get; set; }
+        public string ContentType { get; set; }
+        public int Category { get; set; }
+    }
+}
